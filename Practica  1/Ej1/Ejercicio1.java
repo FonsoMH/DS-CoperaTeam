22261663d7b48849b60f4acd11f3825aeda93307
FonsoMH <halfonsomh04@gmail.com>
2025-03-04 10:11:49 +0100

    Estructura basica de la practica

---
+++ Practica  1/Ej1/Ejercicio1.java
@@ -0,0 +1,18 @@
+//seguir añadiendo clases y to eso
+
+public interface FactoriaCarreraYBicicleta{
+
+    Carrera crearCarrera();
+
+    Bicicleta crearBicicleta();
+
+}
+
+public abstract class Carrera{
+
+}
+
+
+public abstract class Bicicleta{
+
+}
